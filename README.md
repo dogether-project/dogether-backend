3dd1e5f53aa3b2ea80f0548e661df1272c29ac0a
kelly6bf <kelly6bf@gmail.com>
2025-01-22 19:15:05 +0900

    init project

---
+++ README.md
@@ -0,0 +1,2 @@
+# Dogether Backend Repository
+같이 하면 할 수 있다! 가족, 친구, 지인들과 함께하는 데일리 투두 챌린지 앱, `두게더 ✅`
---
+++ src/main/java/site/dogether/DogetherApplication.java
@@ -0,0 +1,12 @@
+package site.dogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogetherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DogetherApplication.class, args);
+    }
+}
