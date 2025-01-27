2f50be0b958d4c5222eda8db4350726e9778ebf5
kelly6bf <kelly6bf@gmail.com>
2025-01-27 22:37:12 +0900

    chore: #1 제약사항 필드 추가

--- src/test/java/site/dogether/docs/RestDocsSupport.java
+++ src/test/java/site/dogether/docs/RestDocsSupport.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.snippet.Attributes;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -16,6 +17,10 @@ public abstract class RestDocsSupport {
     protected final ObjectMapper objectMapper = new ObjectMapper();
 
     protected MockMvc mockMvc;
+
+    protected static Attributes.Attribute constraints(final String value) {
+        return new Attributes.Attribute("constraints", value);
+    }
 
     @BeforeEach
     void setUp(final RestDocumentationContextProvider provider) {
