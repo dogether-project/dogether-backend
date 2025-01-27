c281a6b0abc882c00bd40c3be2637e5135b9a3eb
kelly6bf <kelly6bf@gmail.com>
2025-01-28 06:37:53 +0900

    chore: #1 rest docs 공통 설정 분리

--- src/test/java/site/dogether/docs/RestDocsSupport.java
+++ src/test/java/site/dogether/docs/RestDocsSupport.java
@@ -5,11 +5,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
 import org.springframework.restdocs.snippet.Attributes;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.modifyHeaders;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 
 @ExtendWith(RestDocumentationExtension.class)
 public abstract class RestDocsSupport {
@@ -17,6 +22,7 @@ public abstract class RestDocsSupport {
     protected final ObjectMapper objectMapper = new ObjectMapper();
 
     protected MockMvc mockMvc;
+    protected RestDocumentationResultHandler restDocs;
 
     protected static Attributes.Attribute constraints(final String value) {
         return new Attributes.Attribute("constraints", value);
@@ -24,8 +30,27 @@ public abstract class RestDocsSupport {
 
     @BeforeEach
     void setUp(final RestDocumentationContextProvider provider) {
+        this.restDocs = MockMvcRestDocumentation.document("{class-name}/{method-name}");
         this.mockMvc = MockMvcBuilders.standaloneSetup(initController())
-                .apply(documentationConfiguration(provider))
+                .apply(documentationConfiguration(provider)
+                    .operationPreprocessors()
+                    .withRequestDefaults(
+                        modifyHeaders()
+                            .remove("Content-Length")
+                            .remove("Host"),
+                        prettyPrint())
+                    .withResponseDefaults(
+                        modifyHeaders()
+                            .remove("Content-Length")
+                            .remove("X-Content-Type-Options")
+                            .remove("X-XSS-Protection")
+                            .remove("Cache-Control")
+                            .remove("Pragma")
+                            .remove("Expires")
+                            .remove("X-Frame-Options"),
+                        prettyPrint()))
+                .alwaysDo(restDocs)
+                .alwaysDo(MockMvcResultHandlers.print())
                 .build();
     }
 
