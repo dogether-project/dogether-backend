5c5db8b4eae92235861946e9321f48ff7a37cb71
kelly6bf <kelly6bf@gmail.com>
2025-02-03 02:15:12 +0900

    feat: #17 챌린지 그룹 & 멤버 JPA Entity & Repository 구현

---
+++ src/main/java/site/dogether/challengegroup/infrastructure/entity/ChallengeGroupJpaEntity.java
@@ -0,0 +1,75 @@
+package site.dogether.challengegroup.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import site.dogether.common.audit.entity.BaseTimeEntity;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "challenge_group")
+@Entity
+public class ChallengeGroupJpaEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 30, nullable = false)
+    private String name;
+
+    @Column(name = "maximum_member_count", nullable = false)
+    private int maximumMemberCount;
+
+    @Column(name = "start_at", nullable = false)
+    private LocalDateTime startAt;
+
+    @Column(name = "end_at", nullable = false)
+    private LocalDateTime endAt;
+
+    @Column(name = "maximum_todo_count", nullable = false)
+    private int maximumTodoCount;
+
+    @Column(name = "join_code", length = 20, nullable = false, unique = true)
+    private String joinCode;
+
+    public ChallengeGroupJpaEntity(
+        final String name,
+        final int maximumMemberCount,
+        final LocalDateTime startAt,
+        final LocalDateTime endAt,
+        final int maximumTodoCount,
+        final String joinCode
+    ) {
+        this(
+            null,
+            name,
+            maximumMemberCount,
+            startAt,
+            endAt,
+            maximumTodoCount,
+            joinCode
+        );
+    }
+
+    public ChallengeGroupJpaEntity(
+        final Long id,
+        final String name,
+        final int maximumMemberCount,
+        final LocalDateTime startAt,
+        final LocalDateTime endAt,
+        final int maximumTodoCount,
+        final String joinCode
+    ) {
+        this.id = id;
+        this.name = name;
+        this.maximumMemberCount = maximumMemberCount;
+        this.startAt = startAt;
+        this.endAt = endAt;
+        this.maximumTodoCount = maximumTodoCount;
+        this.joinCode = joinCode;
+    }
+}
---
+++ src/main/java/site/dogether/challengegroup/infrastructure/repository/ChallengeGroupJpaRepository.java
@@ -0,0 +1,7 @@
+package site.dogether.challengegroup.infrastructure.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import site.dogether.challengegroup.infrastructure.entity.ChallengeGroupJpaEntity;
+
+public interface ChallengeGroupJpaRepository extends JpaRepository<ChallengeGroupJpaEntity, Long> {
+}
