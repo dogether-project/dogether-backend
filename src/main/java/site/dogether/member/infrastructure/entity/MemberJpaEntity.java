f8a1f61c790d9e61ff08bf991fde63f1817ef6ca
kelly6bf <kelly6bf@gmail.com>
2025-02-03 01:55:26 +0900

    feat: #17 회원 JPA Entity & Repository 구현

---
+++ src/main/java/site/dogether/member/infrastructure/entity/MemberJpaEntity.java
@@ -0,0 +1,34 @@
+package site.dogether.member.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import site.dogether.common.audit.entity.BaseTimeEntity;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "member")
+@Entity
+public class MemberJpaEntity extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "provider_id", length = 100, nullable = false, unique = true)
+    private String providerId;
+
+    @Column(name = "name", length = 20, nullable = false, unique = true)
+    private String name;
+
+    public MemberJpaEntity(final String providerId, final String name) {
+        this(null, providerId, name);
+    }
+
+    public MemberJpaEntity(final Long id, final String providerId, final String name) {
+        this.id = id;
+        this.providerId = providerId;
+        this.name = name;
+    }
+}
