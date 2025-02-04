159a33d438725ad6bcf653268207e04bffc5df61
Yeongjae Kim <sees77770@gmail.com>
2025-02-05 00:03:24 +0900

    feat: #19 Member 예외 처리 핸들러 추가

---
+++ src/main/java/site/dogether/member/exception/MemberExceptionCode.java
@@ -0,0 +1,16 @@
+package site.dogether.member.exception;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import site.dogether.common.error.ExceptionCode;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PACKAGE)
+public enum MemberExceptionCode implements ExceptionCode {
+
+    INVALID_MEMBER_EXCEPTION("MF-001", "유효하지 않은 회원입니다.");
+
+    private final String value;
+    private final String message;
+}
