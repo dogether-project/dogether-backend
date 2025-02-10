4a99c066257e760a90122cf7e9077d80edd33ab0
kelly6bf <kelly6bf@gmail.com>
2025-02-11 01:46:04 +0900

    feat: #38 s3 presigned url 생성 기능 구현

---
+++ src/main/java/site/dogether/s3/controller/S3Controller.java
@@ -0,0 +1,34 @@
+package site.dogether.s3.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import site.dogether.common.controller.response.ApiResponse;
+import site.dogether.s3.controller.request.IssueS3PresignedUrlsRequest;
+import site.dogether.s3.controller.response.IssueS3PresignedUrlsResponse;
+import site.dogether.s3.controller.response.S3SuccessCode;
+import site.dogether.s3.service.S3Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/s3")
+@RestController
+public class S3Controller {
+
+    private final S3Service s3Service;
+
+    @PostMapping("/presigned-urls")
+    public ResponseEntity<ApiResponse<IssueS3PresignedUrlsResponse>> issueS3PresignedUrls(
+        @RequestBody IssueS3PresignedUrlsRequest request
+    ) {
+        final List<String> s3PresignedUrls = s3Service.issueS3PresignedUrls(request.dailyTodoId(), request.uploadFileTypes());
+        return ResponseEntity.ok(ApiResponse.successWithData(
+            S3SuccessCode.ISSUED_S3_PRESIGNED_URLS,
+            new IssueS3PresignedUrlsResponse(s3PresignedUrls)
+        ));
+    }
+}
