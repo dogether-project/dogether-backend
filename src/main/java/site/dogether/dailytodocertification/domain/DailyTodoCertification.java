3857d6e96fa5743f1ec4a24ea690daba4307651e
kelly6bf <kelly6bf@gmail.com>
2025-02-20 21:30:24 +0900

    fix1 : 데일리 투두 수행 인증글에 공백을 넣을 수 있도록 제약 조건 변경

--- src/main/java/site/dogether/dailytodocertification/domain/DailyTodoCertification.java
+++ src/main/java/site/dogether/dailytodocertification/domain/DailyTodoCertification.java
@@ -12,8 +12,7 @@ import java.time.LocalDateTime;
 @Getter
 public class DailyTodoCertification {
 
-    private static final int MINIMUM_LIMIT_CONTENT_LENGTH = 2;
-    private static final int MAXIMUM_LIMIT_CONTENT_LENGTH = 50;
+    private static final int MAXIMUM_LIMIT_CONTENT_LENGTH = 40;
 
     private final Long id;
     private final DailyTodo dailyTodo;
@@ -52,13 +51,8 @@ public class DailyTodoCertification {
     }
 
     private void validateContent(final String content) {
-        if (content == null || content.isBlank()) {
-            throw new InvalidDailyTodoCertificationException("데일리 투두 수행 인증 본문으로 공백을 입력할 수 없습니다.");
-        }
-
-        if (content.length() < MINIMUM_LIMIT_CONTENT_LENGTH || content.length() > MAXIMUM_LIMIT_CONTENT_LENGTH) {
-            final String exceptionMessage = String.format("데일리 투두 수행 인증 본문은 %d ~ %d길이의 문자열만 가능합니다.",
-                MINIMUM_LIMIT_CONTENT_LENGTH,
+        if (content != null && content.isBlank() && content.length() > MAXIMUM_LIMIT_CONTENT_LENGTH) {
+            final String exceptionMessage = String.format("데일리 투두 수행 인증 본문 길이는 %d까지 가능합니다.",
                 MAXIMUM_LIMIT_CONTENT_LENGTH
             );
             throw new InvalidDailyTodoCertificationException(exceptionMessage);
