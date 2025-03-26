9709e904dbec96f54e17bdb9eca01b446bcaa659
kelly6bf <kelly6bf@gmail.com>
2025-03-26 11:18:02 +0900

    feat: #4 인증된 사용자의 id를 파싱하는 @Authenticated 애노테이션 구현

--- src/main/java/site/dogether/auth/resolver/Authenticated.java
+++ src/main/java/site/dogether/auth/resolver/Authenticated.java
@@ -1,4 +1,4 @@
-package site.dogether.common.config.web.resolver;
+package site.dogether.auth.resolver;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -7,5 +7,5 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.PARAMETER)
-public @interface Authentication {
+public @interface Authenticated {
 }
