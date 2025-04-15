130c7ab905549f75f1e7890dc61303de2edd9fe3
Hwangseoeun <sunny030921@gmail.com>
2025-04-16 03:46:58 +0900

    chore: Rest Docs Overview 수정 - Success Code `CGS-0006` 추가 - `CGS-0005` api명 수정

---
+++ src/main/java/site/dogether/memberactivity/controller/MemberActivityController.java
@@ -0,0 +1,13 @@
+package site.dogether.memberactivity.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/activity")
+@RestController
+public class MemberActivityController {
+
+
+}
