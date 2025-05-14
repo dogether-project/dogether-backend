6eb6e4989ac9a8798ae7223269d223abb981e3f5
Hwangseoeun <sunny030921@gmail.com>
2025-05-14 18:56:22 +0900

    refactor: #83 투두를 누군가 검사할 때 사용자의 통계 값 변경 certificated_count 값 감소, approved_count or rejected_count 증가할 수 있도록 DailyTodoStats에 도메인 역할 추가

--- src/main/java/site/dogether/memberactivity/entity/DailyTodoStats.java
+++ src/main/java/site/dogether/memberactivity/entity/DailyTodoStats.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import site.dogether.common.audit.entity.BaseEntity;
+import site.dogether.dailytodo.entity.DailyTodoStatus;
+import site.dogether.dailytodo.exception.InvalidDailyTodoStatusException;
 import site.dogether.member.entity.Member;
 
 @ToString
@@ -31,4 +33,38 @@ public class DailyTodoStats extends BaseEntity {
 
     @Column(name = "rejected_count", nullable = false, columnDefinition = "INT DEFAULT 0")
     private int rejectedCount = 0;
+
+    public DailyTodoStats(
+            final Long id,
+            final Member member,
+            final int certificatedCount,
+            final int approvedCount,
+            final int rejectedCount
+    ) {
+        this.id = id;
+        this.member = member;
+        this.certificatedCount = certificatedCount;
+        this.approvedCount = approvedCount;
+        this.rejectedCount = rejectedCount;
+    }
+
+    private void increaseCertificatedCount() {
+        this.certificatedCount += 1;
+    }
+
+    public void moveCertificatedToResult(final DailyTodoStatus result) {
+        this.certificatedCount -= 1;
+
+        if(result.equals(DailyTodoStatus.APPROVE)) {
+            this.approvedCount += 1;
+            return;
+        }
+
+        if(result.equals(DailyTodoStatus.REJECT)) {
+            this.rejectedCount += 1;
+            return;
+        }
+
+        throw new InvalidDailyTodoStatusException(String.format("존재하지 않는 데일리 투두 상태입니다. (%s)", result));
+    }
 }
