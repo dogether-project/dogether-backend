1061be68630cd0b73455e8124e3f2ac44d2ebdfa
Hwangseoeun <sunny030921@gmail.com>
2025-05-15 19:59:28 +0900

    feat: #90 사용자 프로필 조회 api 구현 사용자의 이름과 프로필 이미지를 조회할 수 있도록 추가하였습니다.

--- src/main/java/site/dogether/memberactivity/controller/response/MemberActivitySuccessCode.java
+++ src/main/java/site/dogether/memberactivity/controller/response/MemberActivitySuccessCode.java
@@ -10,7 +10,8 @@ public enum MemberActivitySuccessCode implements SuccessCode {
 
     GET_ALL_GROUP_NAMES("CGS-0006", "참여중인 그룹의 이름 목록이 조회되었습니다."),
     GET_GROUP_ACTIVITY_STAT("CGS-0007", "특정 챌린지 그룹의 활동 정보가 조회되었습니다."),
-    GET_MEMBER_ALL_STATS("CGS-0008", "사용자의 모든 인증 목록이 조회되었습니다.");
+    GET_MEMBER_ALL_STATS("CGS-0008", "사용자의 모든 인증 목록이 조회되었습니다."),
+    GET_MEMBER_PROFILE("CGS-0009", "사용자의 프로필이 조회되었습니다.");
 
     private final String value;
     private final String message;
