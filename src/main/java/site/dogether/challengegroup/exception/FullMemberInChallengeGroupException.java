8c1e822a7479de229f4a06caf7e95e9f6a86970e
kelly6bf <kelly6bf@gmail.com>
2025-07-15 19:45:16 +0900

    refactor: #170 ChallengeGroupExceptionHandler 예외 추가 및 수정

--- src/main/java/site/dogether/challengegroup/exception/FullMemberInChallengeGroupException.java
+++
@@ -1,8 +0,0 @@
-package site.dogether.challengegroup.exception;
-
-public class FullMemberInChallengeGroupException extends ChallengeGroupException {
-
-    public FullMemberInChallengeGroupException(final String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/site/dogether/challengegroup/exception/JoiningChallengeGroupNotFoundException.java
@@ -0,0 +1,8 @@
+package site.dogether.challengegroup.exception;
+
+public class JoiningChallengeGroupNotFoundException extends ChallengeGroupException {
+
+    public JoiningChallengeGroupNotFoundException(final String message) {
+        super(message);
+    }
+}
