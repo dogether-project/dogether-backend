52b444c58abd68e8ec9641a28be51951147c4cc6
kelly6bf <kelly6bf@gmail.com>
2025-11-23 02:44:03 +0900

    feat: #229 과거 활동 데이터 CSV 생성 스크립트 초안 작성

---
+++ k6/common/test-data/past-activity-data.js
@@ -0,0 +1,274 @@
+import fs from 'fs';
+import {format} from "fast-csv";
+import {
+    CURRENT_GROUP_RUNNING_DAY, DAY_TODO_CERTIFICATION_PER_MEMBER_COUNT, DAY_TODO_PER_MEMBER_COUNT,
+    getDateNDaysAgo, GROUP_PER_MEMBER_COUNT, MEMBER_COUNT, MEMBER_PER_GROUP_COUNT,
+    PAST_GROUP_RUNNING_DAY,
+    TOTAL_PAST_ACTIVITY_DAY, waitForStreamFinish,
+    calculateNextDate, calculateEndAt, convertDateTimeFormatString, PAST_ACTIVITY_DATA_BASE_PATH, toDateOnly
+} from "./test-data-common.js";
+
+// =========== 상수 ===========
+export const TOTAL_ACTIVITY_CYCLE = Math.ceil(TOTAL_PAST_ACTIVITY_DAY / PAST_GROUP_RUNNING_DAY);
+export const ONE_CYCLE_PER_GROUP_COUNT = MEMBER_COUNT / MEMBER_PER_GROUP_COUNT * GROUP_PER_MEMBER_COUNT;   // 한 사이클에 존재하는 그룹 개수
+export const PAST_GROUP_ACTIVITY_START_AT = getDateNDaysAgo(TOTAL_ACTIVITY_CYCLE * PAST_GROUP_RUNNING_DAY + CURRENT_GROUP_RUNNING_DAY);
+
+// =========== 최종 데이터 개수 ===========
+const TOTAL_DAILY_TODO_CERTIFICATION_COUNT = MEMBER_COUNT * DAY_TODO_CERTIFICATION_PER_MEMBER_COUNT * PAST_GROUP_RUNNING_DAY * GROUP_PER_MEMBER_COUNT * TOTAL_ACTIVITY_CYCLE;
+
+// =========== CSV Stream ===========
+const member_stream = format({ headers: true });
+member_stream.pipe(fs.createWriteStream(`${PAST_ACTIVITY_DATA_BASE_PATH}/member.csv`));
+
+const notification_token_stream = format({ headers: true });
+notification_token_stream.pipe(fs.createWriteStream(`${PAST_ACTIVITY_DATA_BASE_PATH}/notification_token.csv`));
+
+const daily_todo_stats_stream = format({ headers: true });
+daily_todo_stats_stream.pipe(fs.createWriteStream(`${PAST_ACTIVITY_DATA_BASE_PATH}/daily_todo_stats.csv`));
+
+const challenge_group_stream = format({ headers: true });
+challenge_group_stream.pipe(fs.createWriteStream(`${PAST_ACTIVITY_DATA_BASE_PATH}/challenge_group.csv`));
+
+const challenge_group_member_stream = format({ headers: true });
+challenge_group_member_stream.pipe(fs.createWriteStream(`${PAST_ACTIVITY_DATA_BASE_PATH}/challenge_group_member.csv`));
+
+const daily_todo_stream = format({ headers: true });
+daily_todo_stream.pipe(fs.createWriteStream(`${PAST_ACTIVITY_DATA_BASE_PATH}/daily_todo.csv`));
+
+const daily_todo_history_stream = format({ headers: true });
+daily_todo_history_stream.pipe(fs.createWriteStream(`${PAST_ACTIVITY_DATA_BASE_PATH}/daily_todo_history.csv`));
+
+const daily_todo_certification_stream = format({ headers: true });
+daily_todo_certification_stream.pipe(fs.createWriteStream(`${PAST_ACTIVITY_DATA_BASE_PATH}/daily_todo_certification.csv`));
+
+const daily_todo_certification_reviewer_stream = format({ headers: true });
+daily_todo_certification_reviewer_stream.pipe(fs.createWriteStream(`${PAST_ACTIVITY_DATA_BASE_PATH}/daily_todo_certification_reviewer.csv`));
+
+// =========== 캐싱 ===========
+const groupIdsByMember = Array.from({ length: MEMBER_COUNT }, () => []);
+const todoIdsByMember = Array.from({ length: MEMBER_COUNT }, () => []);
+
+// =========== 메인 로직 ===========
+let memberId = 1;  // member & notification_token & daily_todo_stats
+let challengeGroupId = 1;
+let challengeGroupMemberId = 1;
+let dailyTodoId = 1;  // daily_todo & daily_todo_history
+let dailyTodoCertificationId = 1;
+
+async function createTestData() {
+    console.log(`👷[Const Past Activity Data] 과거 활동 테스트 데이터 생성중... (총 과거 진행일 수 : ${TOTAL_ACTIVITY_CYCLE * PAST_GROUP_RUNNING_DAY}일)\n`);
+
+    await createMemberInfoData();
+    await createChallengeAndTodoData();
+
+    console.log(`🎉 테스트 데이터 생성 모두 완료!`);
+}
+
+// member & notification_token & daily_todo_stats 데이터 생성
+async function createMemberInfoData() {
+    console.log("🧑‍🍳 member & notification_token & daily_todo_stats 테스트 데이터 생성중...");
+
+    let memberCreatedAt = PAST_GROUP_ACTIVITY_START_AT;
+    memberCreatedAt = memberCreatedAt.substring(0, 10) + " 06:00:00";
+
+    const dailyTodoCertificationCountPerMember = TOTAL_DAILY_TODO_CERTIFICATION_COUNT / MEMBER_COUNT;
+    const approvedDailyTodoCertificationCountPerMember = Math.ceil(dailyTodoCertificationCountPerMember / 2);
+    const rejectedDailyTodoCertificationCountPerMember = dailyTodoCertificationCountPerMember - approvedDailyTodoCertificationCountPerMember;
+
+    for (let i = 0; i < MEMBER_COUNT; i++) {
+        const currentMemberId = memberId++;
+
+        member_stream.write({
+            id: currentMemberId,
+            provider_id: `pid-${currentMemberId}`,
+            name: `m-${currentMemberId}`,
+            profile_image_url: `http://profile-image.site/${currentMemberId}`,
+            created_at: memberCreatedAt,
+            row_inserted_at: memberCreatedAt,
+            row_updated_at: null
+        });
+
+        notification_token_stream.write({
+            id: currentMemberId,
+            member_id: currentMemberId,
+            token_value: `t-${currentMemberId}`,
+            row_inserted_at: memberCreatedAt,
+            row_updated_at: null
+        });
+
+        daily_todo_stats_stream.write({
+            id: currentMemberId,
+            member_id: currentMemberId,
+            certificated_count: dailyTodoCertificationCountPerMember,
+            approved_count: approvedDailyTodoCertificationCountPerMember,
+            rejected_count: rejectedDailyTodoCertificationCountPerMember,
+            row_inserted_at: memberCreatedAt,
+            row_updated_at: null
+        });
+    }
+
+    member_stream.end();
+    notification_token_stream.end();
+    daily_todo_stats_stream.end();
+
+    await Promise.all([
+        waitForStreamFinish(member_stream),
+        waitForStreamFinish(notification_token_stream),
+        waitForStreamFinish(daily_todo_stats_stream),
+    ]);
+
+    console.log("✅ member & notification_token & daily_todo_stats 테스트 데이터 생성 완료!\n");
+}
+
+// challenge_group & challenge_group_member & daily_todo & daily_todo_history & daily_todo_certification & daily_todo_certification_reviewer 데이터 생성
+async function createChallengeAndTodoData() {
+    console.log("🧑‍🍳 챌린지 그룹 & 투두 & 투두 인증 관련 테스트 데이터 생성중...");
+
+    for (let cycle = 0; cycle < TOTAL_ACTIVITY_CYCLE; cycle++) {
+        let groupStartAtInCycle = calculateNextDate(PAST_GROUP_ACTIVITY_START_AT, cycle, PAST_GROUP_RUNNING_DAY);
+        groupStartAtInCycle = groupStartAtInCycle.substring(0, 10) + " 07:00:00";
+        const groupStartAtInCycleOnlyDate = toDateOnly(groupStartAtInCycle);
+
+        const groupEndAtInCycle = toDateOnly(calculateEndAt(groupStartAtInCycle, PAST_GROUP_RUNNING_DAY + 1));
+        const firstGroupIdInCycle = 1 + cycle * ONE_CYCLE_PER_GROUP_COUNT;
+
+        // 2. challenge_group & challenge_group_member 데이터 생성
+        for (let i = 0; i < ONE_CYCLE_PER_GROUP_COUNT; i++) {
+            const currentChallengeGroupId = challengeGroupId++;
+            challenge_group_stream.write({
+                id: currentChallengeGroupId,
+                name: `g-${currentChallengeGroupId}`,
+                maximum_member_count: MEMBER_PER_GROUP_COUNT,
+                join_code: `jc-${currentChallengeGroupId}`,
+                status: "FINISHED",
+                start_at: groupStartAtInCycleOnlyDate,
+                end_at: groupEndAtInCycle,
+                created_at: groupStartAtInCycle,
+                row_inserted_at: groupStartAtInCycle,
+                row_updated_at: null
+            });
+        }
+
+        let groupId = firstGroupIdInCycle;
+        for (let i = 0; i < GROUP_PER_MEMBER_COUNT; i++) {
+            for (let j = 0; j < MEMBER_COUNT / MEMBER_PER_GROUP_COUNT; j++) {
+                let memberId = 1 + j * MEMBER_PER_GROUP_COUNT;
+                for (let k = 0; k < MEMBER_PER_GROUP_COUNT; k++) {
+                    let currentMemberId = memberId++;
+                    challenge_group_member_stream.write({
+                        id: challengeGroupMemberId++,
+                        challenge_group_id: groupId,
+                        member_id: currentMemberId,
+                        created_at: groupStartAtInCycle,
+                        row_inserted_at: groupStartAtInCycle,
+                        row_updated_at: null
+                    });
+                    groupIdsByMember[currentMemberId - 1].push(groupId);
+                }
+                groupId++;
+            }
+        }
+
+        for (let day = 0; day < PAST_GROUP_RUNNING_DAY; day++) {
+            let currentTodoWrittenAt = new Date(groupStartAtInCycle);
+            currentTodoWrittenAt.setDate(currentTodoWrittenAt.getDate() + day);
+            currentTodoWrittenAt.setHours(8, 0, 0, 0);
+            currentTodoWrittenAt = convertDateTimeFormatString(currentTodoWrittenAt);
+
+            let currentTodoCertifyAt = new Date(currentTodoWrittenAt);
+            currentTodoCertifyAt.setHours(17, 0, 0, 0);
+            currentTodoCertifyAt = convertDateTimeFormatString(currentTodoCertifyAt);
+
+            for (let memberId = 1; memberId <= MEMBER_COUNT; memberId++) {
+                const reviewerId = getReviewerId(memberId);
+                for (let i = 0; i < GROUP_PER_MEMBER_COUNT; i++) {
+                    let reviewStatusToggle = true;
+                    for (let j = 0; j < DAY_TODO_PER_MEMBER_COUNT; j++) {
+                        // 3. daily_todo & daily_todo_history 데이터 생성
+                        const currentTodoId = dailyTodoId++;
+                        daily_todo_stream.write({
+                            id: currentTodoId,
+                            challenge_group_id: groupIdsByMember[memberId - 1][i],
+                            writer_id: memberId,
+                            content: `td=${currentTodoId}`,
+                            status: "CERTIFY_COMPLETED",
+                            written_at: currentTodoWrittenAt,
+                            row_inserted_at: currentTodoWrittenAt,
+                            row_updated_at: null
+                        });
+
+                        daily_todo_history_stream.write({
+                            id: currentTodoId,
+                            daily_todo_id: currentTodoId,
+                            event_time: currentTodoWrittenAt,
+                            row_inserted_at: currentTodoWrittenAt,
+                            row_updated_at: null
+                        });
+                        todoIdsByMember[memberId - 1].push(currentTodoId);
+
+                        // 4. daily_todo_certification & daily_todo_certification_reviewer 데이터 생성
+                        // 현재는 투두 개수 == 인증 개수로 고정했지만 인증 개수만 다르게 하고 싶다면 아래 로직을 별도 루프로 분리해야함.
+                        const currentTodoCertificationId = dailyTodoCertificationId++;
+                        const reviewStatus = reviewStatusToggle ? "APPROVE" : "REJECT";
+                        const reviewFeedBack = reviewStatusToggle ? `와 미쳤다 ㄷㄷ - ${currentTodoCertificationId}` : `그게 최선인가? ㅎ - ${currentTodoCertificationId}`;
+                        reviewStatusToggle = !reviewStatusToggle;
+
+                        daily_todo_certification_stream.write({
+                            id: currentTodoCertificationId,
+                            daily_todo_id: currentTodoId,
+                            content: `tc-${currentTodoId}`,
+                            media_url: `http://certification-media.site/m${memberId}/t${currentTodoId}`,
+                            review_status: reviewStatus,
+                            review_feedback: reviewFeedBack,
+                            created_at: currentTodoCertifyAt,
+                            row_inserted_at: currentTodoCertifyAt,
+                            row_updated_at: null
+                        });
+
+                        daily_todo_certification_reviewer_stream.write({
+                            id: currentTodoCertificationId,
+                            daily_todo_certification_id: currentTodoCertificationId,
+                            reviewer_id: reviewerId,
+                            row_inserted_at: currentTodoCertifyAt,
+                            row_updated_at: null
+                        });
+                    }
+                }
+            }
+        }
+    }
+
+    challenge_group_stream.end();
+    challenge_group_member_stream.end();
+    daily_todo_stream.end();
+    daily_todo_history_stream.end();
+    daily_todo_certification_stream.end();
+    daily_todo_certification_reviewer_stream.end();
+
+    await Promise.all([
+        waitForStreamFinish(challenge_group_stream),
+        waitForStreamFinish(challenge_group_member_stream),
+        waitForStreamFinish(daily_todo_stream),
+        waitForStreamFinish(daily_todo_history_stream),
+        waitForStreamFinish(daily_todo_certification_stream),
+        waitForStreamFinish(daily_todo_certification_reviewer_stream),
+    ]);
+
+    console.log("✅ 챌린지 그룹 & 투두 & 투두 인증 관련 테스트 데이터 생성 완료!\n");
+}
+
+function getReviewerId(memberId) {
+    // 그룹의 시작과 끝 ID를 구함
+    const groupIndex = Math.floor((memberId - 1) / MEMBER_PER_GROUP_COUNT);
+    const startId = groupIndex * MEMBER_PER_GROUP_COUNT + 1;         // 예: 1, 21, 41, ...
+
+    // 그룹 내 offset (0~19)
+    const offset = memberId - startId;
+
+    // 매칭 규칙: 0 <-> 19, 1 <-> 18, ...
+    const reviewerOffset = MEMBER_PER_GROUP_COUNT - 1 - offset;
+
+    return startId + reviewerOffset;
+}
+
+createTestData().then(() => {});
---
+++ k6/common/test-data/test-data-common.js
@@ -0,0 +1,72 @@
+// =========== 상수 ===========
+// 회원 관련 데이터 옵션
+export const MEMBER_COUNT = 400;   // 총 회원 수
+export const DAY_TODO_PER_MEMBER_COUNT = 10;  // 회원이 하루에 작성하는 투두 개수
+export const GROUP_PER_MEMBER_COUNT = 5;  // 회원당 참여 그룹 수
+export const DAY_TODO_CERTIFICATION_PER_MEMBER_COUNT = DAY_TODO_PER_MEMBER_COUNT;  // 회원이 하루 인증하는 투두 인증 개수
+export const MEMBER_PER_GROUP_COUNT = 20;  // 그룹당 참여 회원 수
+
+// 과거 활동 데이터 옵션
+export const PAST_GROUP_RUNNING_DAY = 3;  // 과거 그룹 진행일 수
+export const TOTAL_PAST_ACTIVITY_DAY = 365;  // 총 과거 진행일 수
+export const TOTAL_PAST_ACTIVITY_CYCLE = Math.ceil(TOTAL_PAST_ACTIVITY_DAY / PAST_GROUP_RUNNING_DAY);
+export const PAST_GROUP_ACTIVITY_START_AT = getDateNDaysAgo(TOTAL_PAST_ACTIVITY_CYCLE * PAST_GROUP_RUNNING_DAY + CURRENT_GROUP_RUNNING_DAY);
+
+// 현재 활동 데이터 옵션
+export const CURRENT_GROUP_RUNNING_DAY = 28;  // 현재 그룹 진행일 수
+
+// CSV 파일 옵션
+export const PAST_ACTIVITY_DATA_BASE_PATH = './csv/past-activity-data';
+export const CURRENT_ACTIVITY_DATA_BASE_PATH = './csv/current-activity-data';
+
+// =========== 유틸 함수 ===========
+// n일전 날짜 계산
+export function getDateNDaysAgo(n) {
+    const now = new Date();
+    now.setDate(now.getDate() - n);
+    return convertDateTimeFormatString(now);
+}
+
+export function convertDateTimeFormatString(date = new Date()) {
+    // MySQL DATETIME 형식으로 포맷 (YYYY-MM-DD HH:mm:ss)
+    const pad = (n) => n.toString().padStart(2, '0');
+    return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())} ${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`;
+}
+
+/**
+ * 날짜 정보만 파싱
+ */
+export function toDateOnly(dateTimeString) {
+    if (typeof dateTimeString !== 'string') return dateTimeString;
+
+    // 공백 기준으로 앞부분(날짜)만 사용
+    return dateTimeString.split(' ')[0];
+}
+
+/**
+ * CSV 파일 Stream Flush 대기 Util
+ */
+export function waitForStreamFinish(stream) {
+    return new Promise((resolve, reject) => {
+        stream.on('finish', resolve);
+        stream.on('error', reject);
+    });
+}
+
+/**
+ * startDate에서 cycle * duration일 이후 날짜를 구하는 계산 함수
+ */
+export function calculateNextDate(startDate, cycle, duration) {
+    const d = new Date(startDate);
+    d.setDate(d.getDate() + (cycle * duration));
+    return convertDateTimeFormatString(d);
+}
+
+/**
+ * startAt 날짜에 시작해 duration일 만큼 진행하는 활동의 마지막 활동 일을 구하는 계산 함수
+ */
+export function calculateEndAt(startAt, duration) {
+    const d = new Date(startAt);
+    d.setDate(d.getDate() + (duration - 1));
+    return convertDateTimeFormatString(d);
+}
