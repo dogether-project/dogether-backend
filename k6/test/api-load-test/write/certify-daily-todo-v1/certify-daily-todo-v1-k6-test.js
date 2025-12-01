7f8013879475456cd944e9b7b73a400ea44be99a
kelly6bf <kelly6bf@gmail.com>
2025-12-02 02:34:57 +0900

    chore: #229 데일리 투두 인증 k6 스크립트 수정

--- k6/test/api-load-test/write/certify-daily-todo-v1/certify-daily-todo-v1-k6-test.js
+++ k6/test/api-load-test/write/certify-daily-todo-v1/certify-daily-todo-v1-k6-test.js
@@ -2,7 +2,8 @@ import { sleep } from 'k6';
 import {check} from 'k6';
 import { SharedArray } from 'k6/data';
 import http from 'k6/http';
-import {setRequestHeader, parseResponseBody} from "../../../../common/util/api-util.js";
+import {getOneCertifiableTodoIdPerMember} from "../../../../common/test-data/test-data-common.js";
+import {parseResponseBody, setRequestHeader} from "../../../../common/util/api-util.js";
 import {API_BASE_URL} from "../../../../common/secret/secret.js";
 
 const tokens = new SharedArray('tokens', () => JSON.parse(open('../../../../common/secret/tokens.json')));
@@ -21,38 +22,38 @@ export const options = {
 };
 
 export function setup() {
+    const dailyTodoIds = getOneCertifiableTodoIdPerMember();
+
     console.log("⏰ 10초 대기 시작.");
     sleep(10);
     console.log("✅ 10초 대기 완료.");
+
+    return {dailyTodoIds};
 }
 
-export default function () {
+export default function (data) {
     const vuIndex = __VU - 1;
-    const response = requestApi(vuIndex);
+    const response = requestApi(vuIndex, data.dailyTodoIds[vuIndex]);
     const responseBody = parseResponseBody(response);
-    const responseData = responseBody.data;
 
     check(null, {
         'API HTTP 상태 코드 200': () => response?.status === 200,
         'API 응답 코드 success': () => responseBody?.code === 'success',
-        '응답 데이터 - joinCode 존재': () => responseData?.joinCode !== undefined
     });
 }
 
-function requestApi(vuIndex) {
+function requestApi(vuIndex, dailyTodoId) {
     const timeout = '1800s';
     const headers = setRequestHeader(tokens[vuIndex]);
     const body = JSON.stringify({
-        groupName: `예쁘니 그룹 - ${vuIndex}`,
-        maximumMemberCount: 20,
-        startAt: "TODAY",
-        duration: 28
+        content: `${vuIndex}번 사용자 데일리 투두 인증 땅땅`,
+        mediaUrl: `http://인증-이미지-${vuIndex}.site`
     });
 
-    return http.post(`${API_BASE_URL}/groups`, body, { headers, timeout });
+    return http.post(`${API_BASE_URL}/todos/${dailyTodoId}/certify`, body, { headers, timeout });
 }
 
 export function teardown() {
-    console.log("🧹 5초 후 테스트 데이터 정리 시작.\n");
+    console.log("🧹 5초 후 테스트 데이터 정리 시작.");
     sleep(5);
 }
