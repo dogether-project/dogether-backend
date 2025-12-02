77409f4712b5a924d16ef1eef9d8c45e318acca5
kelly6bf <kelly6bf@gmail.com>
2025-12-02 11:35:00 +0900

    chore: #229 챌린지 그룹 참여 필요 여부 조회 k6 스크립트 수정

--- k6/test/api-load-test/read/check-challenge-group-participation-required-v1.js
+++ k6/test/api-load-test/read/check-challenge-group-participation-required-v1.js
@@ -1,17 +1,20 @@
 import { sleep } from 'k6';
 import {check} from 'k6';
 import { SharedArray } from 'k6/data';
-import {checkGroupParticipatingV1} from "../../../common/api/api-call/v1-api-call.js";
-import {parseResponseBody} from "../../../common/api/util/api-util.js";
+import http from 'k6/http';
+import {parseResponseBody, setRequestHeader} from "../../../common/util/api-util.js";
+import {API_BASE_URL} from "../../../common/secret/secret.js";
 
-const tokens = new SharedArray('tokens', () => JSON.parse(open('../../../secret/tokens.json')));
+const tokens = new SharedArray('tokens', () => JSON.parse(open('../../../common/secret/tokens.json')));
 
 export const options = {
     setupTimeout: '30m',
     scenarios: {
         default: {
             executor: 'per-vu-iterations',
-            vus: 400,
+            vus: 1,
+            // vus: 100,
+            // vus: 400,
             iterations: 1,
             maxDuration: '30m',
         },
@@ -21,18 +24,25 @@ export const options = {
 export function setup() {
     console.log("⏰ 5초 대기 시작.");
     sleep(5);
-    console.log("✅ 5초 대기 완료.\n");
+    console.log("✅ 5초 대기 완료.");
 }
 
 export default function () {
     const vuIndex = __VU - 1;
-    const token = tokens[vuIndex];
+    const response = requestApi(vuIndex);
+    const responseBody = parseResponseBody(response);
+    const responseData = responseBody.data;
 
-    const res = checkGroupParticipatingV1(token);
-    const responseData = parseResponseBody(res).data;
-
-    check(res, {
-        'API 응답 상태 코드 200': (r) => r.status === 200,
-        '응답 데이터 - checkParticipating 존재': () => responseData?.checkParticipating !== undefined
+    check(null, {
+        'API HTTP 상태 코드 200': () => response?.status === 200,
+        'API 응답 코드 success': () => responseBody?.code === 'success',
+        '응답 데이터 - isParticipating 존재': () => responseData?.isParticipating !== undefined
     });
+}
+
+function requestApi(vuIndex) {
+    const timeout = '1800s';
+    const headers = setRequestHeader(tokens[vuIndex]);
+
+    return http.get(`${API_BASE_URL}/groups/participating`, { headers, timeout });
 }
