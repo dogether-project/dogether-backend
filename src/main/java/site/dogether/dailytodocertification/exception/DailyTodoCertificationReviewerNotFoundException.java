1e4689d9b9fde3864cfba64d125c81add456d220
kelly6bf <kelly6bf@gmail.com>
2026-01-01 06:02:32 +0900

    chore: #240 API 문서 수정

---
+++ src/main/java/site/dogether/dailytodocertification/exception/DailyTodoCertificationReviewerNotFoundException.java
@@ -0,0 +1,8 @@
+package site.dogether.dailytodocertification.exception;
+
+public class DailyTodoCertificationReviewerNotFoundException extends DailyTodoCertificationException {
+
+    public DailyTodoCertificationReviewerNotFoundException(final String message) {
+        super(message);
+    }
+}
