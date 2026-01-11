a24a56832d7a432dc7910638dd6bf50ef622ed8e
Hwangseoeun <sunny030921@gmail.com>
2026-01-12 02:30:16 +0900

    refactor: #242 참여중인 특정 챌린지 그룹 활동 통계 조회 API v0 제거

--- src/main/java/site/dogether/memberactivity/controller/v0/MemberActivityControllerV0.java
+++ src/main/java/site/dogether/memberactivity/controller/v0/MemberActivityControllerV0.java
@@ -3,13 +3,11 @@ package site.dogether.memberactivity.controller.v0;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import site.dogether.auth.resolver.Authenticated;
 import site.dogether.common.controller.dto.response.ApiResponse;
-import site.dogether.memberactivity.controller.v1.dto.response.GetGroupActivityStatApiResponseV1;
 import site.dogether.memberactivity.controller.v0.dto.response.GetMemberAllStatsApiResponseV0;
 import site.dogether.memberactivity.controller.v1.dto.response.GetMyProfileApiResponseV1;
 import site.dogether.memberactivity.service.MemberActivityService;
@@ -23,15 +21,6 @@ import static site.dogether.common.controller.dto.response.ApiResponse.success;
 public class MemberActivityControllerV0 {
 
     private final MemberActivityService memberActivityService;
-
-    @GetMapping("/groups/{groupId}/activity")
-    public ResponseEntity<ApiResponse<GetGroupActivityStatApiResponseV1>> getGroupActivityStat(
-            @Authenticated final Long memberId, @PathVariable final Long groupId
-    ) {
-        final GetGroupActivityStatApiResponseV1 groupActivityStat = memberActivityService.getGroupActivityStat(memberId, groupId);
-
-        return ResponseEntity.ok(success(groupActivityStat));
-    }
 
     @GetMapping("/activity")
     public ResponseEntity<ApiResponse<GetMemberAllStatsApiResponseV0>> getMemberAllStats(
