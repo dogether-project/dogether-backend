3c255f2e2a0759a11a72cbcb26cf98465b5e6ccc
kelly6bf <kelly6bf@gmail.com>
2026-05-06 16:19:37 +0900

    feat: #266 테스트 데이터 옵션 수정

--- k6/common/test-data/test-data-common.js
+++ k6/common/test-data/test-data-common.js
@@ -9,13 +9,13 @@ export const DAY_TODO_CERTIFICATION_PER_MEMBER_COUNT = DAY_TODO_PER_MEMBER_COUNT
 
 // 과거 & 현재 활동 데이터 옵션
 export const PAST_GROUP_PER_MEMBER_COUNT = 5;  // 회원당 과거 활동 참여 그룹 수
-export const CURRENT_FOR_READ_GROUP_PER_MEMBER_COUNT = 5;  // 회원당 현재 활동 참여 그룹 수
-export const CURRENT_FOR_WRITE_GROUP_PER_MEMBER_COUNT = 3;  // 회원당 현재 활동 참여 그룹 수
+export const CURRENT_FOR_READ_GROUP_PER_MEMBER_COUNT = 5;  // 회원당 현재 활동 참여 그룹 수 (읽기 테스트)
+export const CURRENT_FOR_WRITE_GROUP_PER_MEMBER_COUNT = 3;  // 회원당 현재 활동 참여 그룹 수 (쓰기 테스트)
 
 export const PAST_GROUP_RUNNING_DAY = 3;  // 과거 그룹 진행일 수
 export const CURRENT_GROUP_RUNNING_DAY = 28;  // 현재 그룹 진행일 수
 
-export const PAST_TOTAL_ACTIVITY_DAY = 30 * 6;  // 총 과거 진행일 수
+export const PAST_TOTAL_ACTIVITY_DAY = 30 * 12;  // 총 과거 진행일 수
 export const PAST_TOTAL_ACTIVITY_CYCLE = Math.ceil(PAST_TOTAL_ACTIVITY_DAY / PAST_GROUP_RUNNING_DAY);
 export const PAST_ONE_CYCLE_PER_GROUP_COUNT = MEMBER_COUNT / MEMBER_PER_GROUP_COUNT * PAST_GROUP_PER_MEMBER_COUNT;   // 한 사이클에 존재하는 그룹 개수
 
